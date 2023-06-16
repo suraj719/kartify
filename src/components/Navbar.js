e8cd86b4b0b562f3d1a7ebea472e5c7d1a9e9d83
suraj pro <71000909+suraj719@users.noreply.github.com>
2023-06-16 23:20:16 +0530

    added redux

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import "./home.css";
 export default function Navbar() {
-    let items = JSON.parse(localStorage.getItem("items")) || []
-    var totalqty = items.reduce((acc,i) => {
-        return acc+i.quantity
-    },0)
+    const items = useSelector(state=>state.cart.products)
+    // var totalqty = items.reduce((acc,i) => {
+    //     return acc+i.quantity
+    // },0)
+    const totalqty = items.length
   return (
     <div>
     <nav className='navbar navbar-expand-md'>
