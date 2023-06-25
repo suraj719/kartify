0fc0996bbbb6fd62dc5ccc64f6142de9cb0db5d7
suraj pro <71000909+suraj719@users.noreply.github.com>
2023-06-25 14:32:12 +0530

    added test server

--- server/server.js
+++ server/server.js
@@ -8,9 +8,9 @@ app.use(express.json())
 app.use(cors())
 app.use("/api/v1/users",users)
 
-// app.get("/",(req,res) => {
-//     res.send("s")
-// })
+app.get("/",(req,res) => {
+    res.send("hello")
+})
 const port = process.env.PORT || 5000
 
 const start = async () => {
